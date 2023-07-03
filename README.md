6bdc457cd5028bfd973bd167cd4ec967c94cd796
chels <chelsleonhardt@gmail.com>
2023-07-03 08:04:20 -0400

    Finalized validation

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 using javascript to validate form
 
 Photo by Alexander Grey: https://www.pexels.com/photo/pink-doughnut-with-colorful-sprinkles-intilt-shift-lens-3784440/
+
+<a target="_blank" href="https://icons8.com/icon/xVhrIJR16FZu/high-five">High Five</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
--- script.js
+++ script.js
@@ -11,6 +11,17 @@ const errorElement = document.getElementById('error');
 var re =
     /^(([a-zA-Z0-9]+)|([a-zA-Z0-9]+((?:\_[a-zA-Z0-9]+)|(?:\.[a-zA-Z0-9]+))*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-zA-Z]{2,6}(?:\.[a-zA-Z]{2})?)$)/;
 
+confirmpassword.addEventListener('mouseout', (e) => {
+    if (confirmpassword.value != password.value || password.value == null) {
+        password.style.borderColor = '#ff54ad';
+        confirmpassword.style.borderColor = '#ff54ad';
+    }
+    if (confirmpassword.value === password.value) {
+        password.style.borderColor = '#54ffc0';
+        confirmpassword.style.borderColor = '#54ffc0';
+    }
+});
+
 form.addEventListener('submit', (e) => {
     let messages = [];
 
@@ -18,7 +29,7 @@ form.addEventListener('submit', (e) => {
         messages.push('Please enter a valid email ex: myname@gmail.com');
     }
 
-    if (isNaN(zip.value)) {
+    if (isNaN(zip.value) || zip.value < 0) {
         messages.push('Please enter a valid zip code ex. 90210');
     }
 
@@ -31,6 +42,7 @@ form.addEventListener('submit', (e) => {
     if (messages.length > 0) {
         e.preventDefault();
         errorElement.innerText = messages.join(', ');
+        errorElement.style.display = 'block';
     }
     if (messages.length == 0) {
         highFive();
